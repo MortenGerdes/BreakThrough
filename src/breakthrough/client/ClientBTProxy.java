129da85f470804b9271c40033729021f64f298ab
Morten <mortengerdes7000@gmail.com>
2017-11-20 10:19:55 +0100

    <Unstable build> Added getColor method to hopefully work. Haven't ran it yet.

--- src/breakthrough/client/ClientBTProxy.java
+++ src/breakthrough/client/ClientBTProxy.java
@@ -1,6 +1,38 @@
 package breakthrough.client;
 
+import breakthrough.domain.Breakthrough;
+import breakthrough.domain.Color;
+import breakthrough.domain.Move;
+import breakthrough.domain.Position;
 import frs.broker.ClientProxy;
+import frs.broker.Constants;
+import frs.broker.Requestor;
 
-public class ClientBTProxy implements ClientProxy {
+public class ClientBTProxy implements ClientProxy, Breakthrough {
+    private final Requestor requestor;
+
+    public ClientBTProxy(Requestor requestor)
+    {
+        this.requestor = requestor;
+    }
+
+    @Override
+    public Color getPieceAt(Position p) {
+        return requestor.sendRequestAndAwaitReply("1", Constants.GET_PIECE_AT_SERVER, Color.class, p);
+    }
+
+    @Override
+    public Color getPlayerInTurn() {
+        return null;
+    }
+
+    @Override
+    public Color getWinner() {
+        return null;
+    }
+
+    @Override
+    public boolean move(Move move) {
+        return false;
+    }
 }
--- src/breakthrough/marshall/BTInvoker.java
+++ src/breakthrough/marshall/BTInvoker.java
@@ -1,11 +1,39 @@
 package breakthrough.marshall;
 
+import breakthrough.domain.Breakthrough;
+import breakthrough.domain.Color;
+import breakthrough.domain.Position;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
+import frs.broker.Constants;
 import frs.broker.Invoker;
 import frs.broker.ReplyObject;
 
+import javax.servlet.http.HttpServletResponse;
+
 public class BTInvoker implements Invoker {
+    private final Breakthrough breakthrough;
+    private final Gson gson;
+
+    public BTInvoker(Breakthrough breakthrough) {
+        this.breakthrough = breakthrough;
+        this.gson = new Gson();
+    }
+
     @Override
-    public ReplyObject handleRequest(String objectId, String operationName, String payload) {
+    public ReplyObject handleRequest(String objectId, String operationName, String payload)
+    {
+        ReplyObject reply = null;
+        JsonParser parser = new JsonParser();
+        JsonArray array = parser.parse(payload).getAsJsonArray();
+
+        if(operationName.equals(Constants.GET_PIECE_AT_SERVER))
+        {
+            Position pos = gson.fromJson(array.get(0), Position.class);
+            Color color = breakthrough.getPieceAt(pos);
+            reply = new ReplyObject(HttpServletResponse.SC_OK, gson.toJson(color));
+        }
         return null;
     }
 }
